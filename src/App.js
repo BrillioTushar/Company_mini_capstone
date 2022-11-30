d98f39af1e34e62b861214c5bac620af180a72d4
Tanishq Sahay <tanishqsahay30@gmail.com>
2022-11-30 12:11:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './Components/Login'
 import SignUp from './Components/SignUp'
 import Home from './Components/Home'
+import { Layouts } from './Components/Layouts';
+
 function App() {
   return (
     <Router>
@@ -17,8 +19,10 @@ function App() {
               <Route path="/log-in" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
               <Route path="/sign-up/home" element={<Home />} />
+                
             </Routes>
           </div>
+<layouts/>
         </div>
       </div>
     </Router>
