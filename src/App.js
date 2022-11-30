0af80291d9804e261fe26365408641665f8fbd71
mayank-kedia <119394177+mayank-kedia@users.noreply.github.com>
2022-11-30 09:28:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,38 +3,20 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './Components/Login'
-import SignUp from './Components/Signup'
+import SignUp from './Components/SignUp'
+import Home from './Components/Home'
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/log-in'}>
-              BRILLIO
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/log-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
               <Route exact path="/" element={<Login />} />
               <Route path="/log-in" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/sign-up/home" element={<Home />} />
             </Routes>
           </div>
         </div>
