bfb38fc57ba6371e441526fce72cbaec59823d29
NehaBrillio <119393634+NehaBrillio@users.noreply.github.com>
2022-12-02 14:08:42 +0530

    Add files via upload

---
+++ src/Components/QuizPage.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './QuizPage.css'
+import Layout from './Layout'
+
+
+function QuizPage() {
+  
+  return (
+    <div className="quiz-outer">
+      
+      <Layout/>
+     
+    </div>  
+  )
+}
+
+export default QuizPage
